91c29a27add9957257dffdc59db7eb9f9359f7ce
Van Ortega <ortegavan@live.com>
2022-05-28 20:20:02 -0300

    Encontro remoto 2

--- ChapterAPI/Controllers/LivroController.cs
+++ ChapterAPI/Controllers/LivroController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChapterAPI.Models;
 using ChapterAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChapterAPI.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     public class LivroController : Controller
     {
@@ -23,6 +25,68 @@ namespace ChapterAPI.Controllers
             try
             {
                 return Ok(_repository.Listar());
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                var livro = _repository.BuscarPorId(id);
+
+                if (livro == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livro);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody]Livro livro)
+        {
+            try
+            {
+                _repository.Cadastrar(livro);
+                return StatusCode(201);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, [FromBody]Livro livro)
+        {
+            try
+            {
+                _repository.Atualizar(id, livro);
+                return StatusCode(202);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            try
+            {
+                _repository.Excluir(id);
+                return StatusCode(204);
             }
             catch
             {
--- ChapterAPI/Repositories/LivroRepository.cs
+++ ChapterAPI/Repositories/LivroRepository.cs
@@ -17,6 +17,44 @@ namespace ChapterAPI.Repositories
         {
 			return _context.Livros.ToList();
         }
+
+		public Livro BuscarPorId(int id)
+        {
+			return _context.Livros.Find(id);
+        }
+
+		public void Cadastrar(Livro livro)
+        {
+			_context.Livros.Add(livro);
+			_context.SaveChanges();
+        }
+
+		public void Atualizar(int id, Livro livro)
+        {
+			var livroBuscado = _context.Livros.Find(id);
+
+			if (livroBuscado != null)
+            {
+				livroBuscado.Id = id;
+				livroBuscado.Titulo = livro.Titulo;
+				livroBuscado.QuantidadePaginas = livro.QuantidadePaginas;
+				livroBuscado.Disponivel = livro.Disponivel;
+
+				_context.Livros.Update(livroBuscado);
+				_context.SaveChanges();
+            }
+        }
+
+		public void Excluir(int id)
+        {
+			var livro = _context.Livros.Find(id);
+
+			if (livro != null)
+            {
+				_context.Livros.Remove(livro);
+				_context.SaveChanges();
+            }
+        }
 	}
 }
 
