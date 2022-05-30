f92fcdcd6abf8a93118eba5d49838a4ac8100e76
Van Ortega <ortegavan@live.com>
2022-05-30 04:35:07 -0300

    Encontros 3 e 4

---
+++ ExoAPI/Controllers/ProjetosController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExoAPI.Models;
+using ExoAPI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExoAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProjetosController : Controller
+    {
+        private readonly ProjetoRepository _repository;
+
+        public ProjetosController(ProjetoRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_repository.Listar());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                var projeto = _repository.BuscarPorId(id);
+
+                return (projeto != null) ? Ok(projeto) : NotFound("Projeto não encontrado");
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody]Projeto projeto)
+        {
+            try
+            {
+                return Ok(_repository.Cadastrar(projeto));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+                
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, [FromBody]Projeto projeto)
+        {
+            try
+            {
+                return Ok(_repository.Atualizar(id, projeto));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            try
+            {
+                if (_repository.Excluir(id))
+                {
+                    return Ok("Projeto removido");
+                }
+
+                return NotFound("Projeto não encontrado");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
---
+++ ExoAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExoAPI.Models;
+using ExoAPI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExoAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]    
+    public class UsuariosController : Controller
+    {
+        private readonly UsuarioRepository _repository;
+
+        public UsuariosController(UsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_repository.Listar());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+       
+        [HttpGet("{id}")]
+        [Authorize]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                var usuario = _repository.BuscarPorId(id);
+
+                return (usuario != null) ? Ok(usuario) : NotFound("Usuário não encontrado");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody]Usuario usuario)
+        {
+            try
+            {
+                return Ok(_repository.Cadastrar(usuario));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+                
+        [HttpPut("{id}")]
+        [Authorize(Roles = "1")]
+        public IActionResult Atualizar(int id, [FromBody]Usuario usuario)
+        {
+            try
+            {
+                return Ok(_repository.Atualizar(id, usuario));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+                
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "1")]
+        public IActionResult Excluir(int id)
+        {
+            try
+            {
+                if (_repository.Excluir(id))
+                {
+                    return Ok("Usuário removido");
+                }
+
+                return NotFound("Usuário não encontrado");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
